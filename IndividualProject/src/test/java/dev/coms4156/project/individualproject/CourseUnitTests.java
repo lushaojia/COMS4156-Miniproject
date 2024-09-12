cf871669442bd79978b4bfa7edacdeac94f0dffa
lushaojia <71670653+lushaojia@users.noreply.github.com>
2024-09-12 14:48:32 -0400

    test: implement tests for each class in src (#2)
    * style: fix style issues according to the Google Java style guide
    
    * test: implement unit tests for every method in Course
    
    * test: implement tests for RouteController
    
    * test: implement tests for MyFileDatabase
    
    * test: implement tests for IndividualProjectApplication

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
@@ -1,8 +1,10 @@
 package dev.coms4156.project.individualproject;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
@@ -13,12 +15,54 @@ import org.springframework.test.context.ContextConfiguration;
 @SpringBootTest
 @ContextConfiguration
 public class CourseUnitTests {
+  /**
+   * A course instance used for testing.
+   */
+  private Course testCourse;
 
-  @BeforeAll
-  public static void setupCourseForTesting() {
-    testCourse = new Course("Griffin Newbold", "417 IAB", "11:40-12:55", 250);
+  /**
+   * Create a Course instance for testing.
+   */
+  @BeforeEach
+  public void setupCourseForTesting() {
+    testCourse = new Course(
+        "Griffin Newbold",
+        "417 IAB",
+        "11:40-12:55",
+        250);
   }
 
+  /*
+   * Testing strategy
+   *
+   * enrollStudent, dropStudent:
+   *  - partition on whether the student can be successfully enrolled/dropped: yes, no
+   *  - partition on number of enrolled students: =0, (0, capacity), =capacity
+   *
+   * reassignInstructor, reassignLocation, reassignTime:
+   *  - partition on new instructor/location/time: same as before, not
+   *
+   * isCourseFull:
+   *  - partition on course: is full, not
+   *  - - partition on number of enrolled students: =0, (0, capacity), =capacity
+   */
+  @Test
+  public void getCourseLocationTest() {
+    String expectedLocation = "417 IAB";
+    assertEquals(expectedLocation, testCourse.getCourseLocation());
+  }
+
+  @Test
+  public void getInstructorNameTest() {
+    String expectedInstructor = "Griffin Newbold";
+    assertEquals(expectedInstructor, testCourse.getInstructorName());
+  }
+
+  @Test
+  public void getCourseTimeSlotTest() {
+    String expectedTimeSlot = "11:40-12:55";
+    assertEquals(expectedTimeSlot, testCourse.getCourseTimeSlot());
+  }
 
   @Test
   public void toStringTest() {
@@ -26,9 +70,79 @@ public class CourseUnitTests {
     assertEquals(expectedResult, testCourse.toString());
   }
 
-  /**
-   * The test course instance used for testing.
-   */
-  public static Course testCourse;
+  @Test
+  public void enrollStudentTest() {
+    assertTrue(testCourse.enrollStudent(),
+        "Expected student to be successfully enrolled in course.");
+
+    testCourse.setEnrolledStudentCount(1);
+    assertTrue(testCourse.enrollStudent(),
+        "Expected student to be successfully enrolled in course.");
+
+    testCourse.setEnrolledStudentCount(250);
+    assertFalse(testCourse.enrollStudent(),
+        "Should not be able to enroll student in course at full capacity.");
+  }
+
+  @Test
+  public void dropStudentTest() {
+    assertFalse(testCourse.dropStudent(),
+        "Should not be able to drop student from empty course.");
+
+    testCourse.setEnrolledStudentCount(1);
+    assertTrue(testCourse.dropStudent(),
+        "Should be able to drop a student from the course.");
+
+    testCourse.setEnrolledStudentCount(250);
+    assertTrue(testCourse.dropStudent(),
+        "Should be able to drop a student from the course.");
+  }
+
+  @Test
+  public void reassignInstructorTest() {
+    testCourse.reassignInstructor("Griffin Newbold");
+    String expectedResult = "\nInstructor: Griffin Newbold; Location: 417 IAB; Time: 11:40-12:55";
+    assertEquals(expectedResult, testCourse.toString());
+
+    testCourse.reassignInstructor("Max Goldman");
+    String expectedResult2 = "\nInstructor: Max Goldman; Location: 417 IAB; Time: 11:40-12:55";
+    assertEquals(expectedResult2, testCourse.toString());
+  }
+
+  @Test
+  public void reassignLocation() {
+    testCourse.reassignLocation("417 IAB");
+    String expectedResult = "\nInstructor: Griffin Newbold; Location: 417 IAB; Time: 11:40-12:55";
+    assertEquals(expectedResult, testCourse.toString());
+
+    testCourse.reassignLocation("12-100");
+    String expectedResult2 = "\nInstructor: Griffin Newbold; Location: 12-100; Time: 11:40-12:55";
+    assertEquals(expectedResult2, testCourse.toString());
+  }
+
+  @Test
+  public void reassignTime() {
+    testCourse.reassignTime("11:40-12:55");
+    String expectedResult = "\nInstructor: Griffin Newbold; Location: 417 IAB; Time: 11:40-12:55";
+    assertEquals(expectedResult, testCourse.toString());
+
+    testCourse.reassignTime("9:30-11");
+    String expectedResult2 = "\nInstructor: Griffin Newbold; Location: 417 IAB; Time: 9:30-11";
+    assertEquals(expectedResult2, testCourse.toString());
+  }
+
+  @Test
+  public void isCourseFullTest() {
+    assertFalse(testCourse.isCourseFull(),
+        "Expected new course to have no enrolled students.");
+
+    testCourse.setEnrolledStudentCount(249);
+    assertFalse(testCourse.isCourseFull(),
+        "Expected course to not be full.");
+
+    testCourse.setEnrolledStudentCount(250);
+    assertTrue(testCourse.isCourseFull(),
+        "Expected course to be full.");
+  }
 }
 
---
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/MyFileDatabaseUnitTests.java
@@ -0,0 +1,173 @@
+package dev.coms4156.project.individualproject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This class contains the unit tests for the MyFileDatabase class.
+ */
+public class MyFileDatabaseUnitTests {
+  /**
+   * MyFileDatabase instances used for testing.
+   */
+  public static MyFileDatabase fileDatabase;
+  public static MyFileDatabase invalidObjectFileDatabase;
+  public static MyFileDatabase emptyFileDatabase;
+
+  /**
+   * Create the following kinds of MyFileDatabase instances for testing:
+   * a regular one, one with a file containing an invalid object (i.e. not a hash map),
+   * and one with an empty file path.
+   */
+  @BeforeAll
+  public static void setUpForTesting() {
+    // Create a file database with empty file path for testing
+    emptyFileDatabase = new MyFileDatabase(1, "");
+
+    // Create a file that stores an invalid (not-hashmap) object type
+    String invalidObjectFilePath = "./invalidObject.txt";
+    List<String> invalidObject = new ArrayList<>();
+    invalidObject.add("gudetama");
+    invalidObject.add("kuromi");
+    invalidObject.add("keroppi");
+    try {
+      ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(invalidObjectFilePath));
+      out.writeObject(invalidObject);
+      System.out.println("Invalid object for testing has been serialized to file.");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    invalidObjectFileDatabase = new MyFileDatabase(1, invalidObjectFilePath);
+
+    // Set up
+    fileDatabase = new MyFileDatabase(1, "./testMyFileDatabase.txt");
+    HashMap<String, Department> departmentMap = new HashMap<>();
+    HashMap<String, Course> courses = new HashMap<String, Course>();
+    Course econ3211 = new Course(
+        "Murat Yilmaz",
+        "310 FAY",
+        "4:10-5:25",
+        96);
+    econ3211.setEnrolledStudentCount(11);
+
+    courses.put("3211", econ3211);
+    Department econDepartment = new Department(
+        "ECON",
+        courses,
+        "Michael Woodford",
+        0
+    );
+    departmentMap.put("ECON", econDepartment);
+    fileDatabase.setMapping(departmentMap);
+  }
+
+  @Test
+  public void setMappingTest() {
+    HashMap<String, Department> departmentMap = new HashMap<>();
+    HashMap<String, Course> courses = new HashMap<>();
+    Course econ3211 = new Course(
+        "Murat Yilmaz",
+        "310 FAY",
+        "4:10-5:25",
+        96);
+    econ3211.setEnrolledStudentCount(11);
+    courses.put("3211", econ3211);
+    Department econDepartment = new Department(
+        "ECON",
+        courses,
+        "Michael Woodford",
+        0
+    );
+    departmentMap.put("ECON", econDepartment);
+    MyFileDatabase database = new MyFileDatabase(1, "");
+    database.setMapping(departmentMap);
+    assertEquals("For the ECON department:\n"
+            + "ECON 3211:\n"
+            + "Instructor: Murat Yilmaz; Location: 310 FAY; Time: 4:10-5:25",
+        database.toString());
+  }
+
+  @Test
+  public void getDepartmentMappingTest() {
+    HashMap<String, Department> expectedDepartmentMap = new HashMap<>();
+    HashMap<String, Course> courses = new HashMap<>();
+    Course econ3211 = new Course(
+        "Murat Yilmaz",
+        "310 FAY",
+        "4:10-5:25",
+        96);
+    econ3211.setEnrolledStudentCount(11);
+
+    courses.put("3211", econ3211);
+    Department econDepartment = new Department(
+        "ECON",
+        courses,
+        "Michael Woodford",
+        0
+    );
+    expectedDepartmentMap.put("ECON", econDepartment);
+
+    assertEquals(expectedDepartmentMap.get("ECON").toString(),
+        fileDatabase.getDepartmentMapping().get("ECON").toString());
+  }
+
+  @Test
+  public void toStringTest() {
+    assertEquals("For the ECON department:\n"
+            + "ECON 3211:\n"
+            + "Instructor: Murat Yilmaz; Location: 310 FAY; Time: 4:10-5:25",
+        fileDatabase.toString());
+  }
+
+  @Test
+  public void serializeAndDeSerializeObjectTest() {
+    HashMap<String, Department> expectedDepartmentMap = new HashMap<>();
+    HashMap<String, Course> courses = new HashMap<>();
+    Course econ3211 = new Course(
+        "Murat Yilmaz",
+        "310 FAY",
+        "4:10-5:25",
+        96);
+    econ3211.setEnrolledStudentCount(11);
+
+    courses.put("3211", econ3211);
+    Department econDepartment = new Department(
+        "ECON",
+        courses,
+        "Michael Woodford",
+        0
+    );
+    expectedDepartmentMap.put("ECON", econDepartment);
+
+    fileDatabase.saveContentsToFile();
+    assertEquals(expectedDepartmentMap.get("ECON").toString(),
+        fileDatabase.deSerializeObjectFromFile().get("ECON").toString());
+  }
+
+  @Test
+  public void deSerializeObjectFromFileInvalidObjectTypeTest() {
+    try {
+      invalidObjectFileDatabase.deSerializeObjectFromFile();
+    } catch (IllegalArgumentException e) {
+      assertTrue(true);
+    }
+    fail();
+  }
+
+  @Test
+  public void deSerializeObjectFromFileThrowsExceptionTest() {
+    assertNull(emptyFileDatabase.deSerializeObjectFromFile());
+  }
+
+}
